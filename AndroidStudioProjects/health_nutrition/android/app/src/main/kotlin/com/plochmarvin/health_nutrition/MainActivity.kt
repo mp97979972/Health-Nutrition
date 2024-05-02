0480c3ab594892958376a7887a7e4c94dd18ff66
Marvin Ploch <m.ploch@mannheim-medizinelektronik.de>
2024-05-02 11:21:49 +0200

    initial commit

---
+++ AndroidStudioProjects/health_nutrition/android/app/src/main/kotlin/com/plochmarvin/health_nutrition/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.plochmarvin.health_nutrition
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ AndroidStudioProjects/health_nutrition/lib/assets/constants/constant_colors.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+
+const Color darkGrey = Color(0xff292929);
+const Color gold = Color(0xffe7c456);
+const Color beige = Color(0xffddb982);
+
+const Color primaryLight = Color(0xff735C00);
+const Color onPrimaryLight = Color(0xffFFFFFF);
+const Color primaryContainerLight = Color(0xffFFE086);
+const Color onPrimaryContainerLight = Color(0xff231B00);
+const Color secondaryLight = Color(0xff006874);
+const Color onSecondaryLight = Color(0xffFFFFFF);
+const Color secondaryContainerLight = Color(0xff97F0FF);
+const Color onSecondaryContainerLight = Color(0xff001F24);
+const Color tertiaryLight = Color(0xff7F5700);
+const Color onTertiaryLight = Color(0xffFFFFFF);
+const Color tertiaryContainerLight = Color(0xffFFDEAD);
+const Color onTertiaryContainerLight = Color(0xff281900);
+const Color outlineLight = Color(0xff7D7667);
+const Color backgroundLight = Color(0xffFFFBFF);
+const Color onBackgroundLight = Color(0xff1E1B16);
+const Color surfaceLight = Color(0xffFFF8F0);
+const Color onSurfaceLight = Color(0xff1E1B16);
+const Color surfaceVariantLight = Color(0xffEBE2CF);
+const Color onSurfaceVariantLight = Color(0xff4C4639);
+const Color inverseSurfaceLight = Color(0xff33302A);
+const Color inverseOnSurfaceLight = Color(0xffF7F0E7);
+const Color inversePrimaryLight = Color(0xffE8C349);
+const Color shadowLight = Color(0xff000000);
+const Color surfaceTintLight = Color(0xff735C00);
+const Color outlineVariantLight = Color(0xffCEC6B4);
+const Color surfaceContainerHighestLight = Color(0xffE8E2D9);
+const Color surfaceContainerHighLight = Color(0xffEEE7DE);
+const Color surfaceContainerLight = Color(0xffFFF8F0);
+const Color surfaceContainerLowLight = Color(0xffF9F3E9);
+const Color surfaceContainerLowestLight = Color(0xffFFFFFF);
+const Color surfaceBrightLight = Color(0xffFFF8F0);
+const Color surfaceDimLight = Color(0xffDFD9D0);
+const Color errorLight = Color(0xffBA1A1A);
+const Color onErrorLight = Color(0xffFFFFFF);
+
+const Color primaryDark = Color(0xffE8C349);
+const Color onPrimaryDark = Color(0xff3C2F00);
+const Color primaryContainerDark = Color(0xff574500);
+const Color onPrimaryContainerDark = Color(0xffFFE086);
+const Color secondaryDark = Color(0xff4FD8EB);
+const Color onSecondaryDark = Color(0xff00363D);
+const Color secondaryContainerDark = Color(0xff97F0FF);
+const Color onSecondaryContainerDark = Color(0xff004F58);
+const Color tertiaryDark = Color(0xffFBBB4A);
+const Color onTertiaryDark = Color(0xff432C00);
+const Color tertiaryContainerDark = Color(0xff604100);
+const Color onTertiaryContainerDark = Color(0xffFFDEAD);
+const Color outlineDark = Color(0xff989080);
+const Color backgroundDark = Color(0xff1E1B16);
+const Color onBackgroundDark = Color(0xffE8E2D9);
+const Color surfaceDark = Color(0xff15130E);
+const Color onSurfaceDark = Color(0xffE8E2D9);
+const Color surfaceVariantDark = Color(0xff4C4639);
+const Color onSurfaceVariantDark = Color(0xffCEC6B4);
+const Color inverseSurfaceDark = Color(0xffE8E2D9);
+const Color inverseOnSurfaceDark = Color(0xff1E1B16);
+const Color inversePrimaryDark = Color(0xff735C00);
+const Color shadowDark = Color(0xff000000);
+const Color surfaceTintDark = Color(0xffE8C349);
+const Color outlineVariantDark = Color(0xff4C4639);
+const Color surfaceContainerHighestDark = Color(0xff37342E);
+const Color surfaceContainerHighDark = Color(0xff2C2A24);
+const Color surfaceContainerDark = Color(0xff221F1A);
+const Color surfaceContainerLowDark = Color(0xff1E1B16);
+const Color surfaceContainerLowestDark = Color(0xff100E09);
+const Color surfaceBrightDark = Color(0xff3C3933);
+const Color surfaceDimDark = Color(0xff15130E);
+const Color errorDark = Color(0xffF2B8B5);
+const Color onErrorDark = Color(0xffFFFFFF);
+
+const lightColorScheme = ColorScheme(
+  brightness: Brightness.light,
+  primary: primaryLight,
+  onPrimary: onPrimaryLight,
+  primaryContainer: primaryContainerLight,
+  onPrimaryContainer: onPrimaryContainerLight,
+  secondary: secondaryLight,
+  onSecondary: onSecondaryLight,
+  secondaryContainer: secondaryContainerLight,
+  onSecondaryContainer: onSecondaryContainerLight,
+  tertiary: tertiaryLight,
+  onTertiary: onTertiaryLight,
+  tertiaryContainer: tertiaryContainerLight,
+  error: errorLight,
+  onError: onErrorLight,
+  background: backgroundLight,
+  onBackground: onBackgroundLight,
+  surface: surfaceLight,
+  onSurface: onSurfaceLight,
+  surfaceVariant: surfaceVariantLight,
+  onSurfaceVariant: onSurfaceVariantLight,
+  outline: outlineLight,
+  outlineVariant: outlineVariantLight,
+  shadow: shadowLight,
+  inverseSurface: inverseSurfaceLight,
+  inversePrimary: inversePrimaryLight,
+  surfaceTint: surfaceTintLight,
+);
+
+const darkColorScheme = ColorScheme(
+  brightness: Brightness.dark,
+  primary: primaryDark,
+  onPrimary: onPrimaryDark,
+  primaryContainer: primaryContainerDark,
+  onPrimaryContainer: onPrimaryContainerDark,
+  secondary: secondaryDark,
+  onSecondary: onSecondaryDark,
+  secondaryContainer: secondaryContainerDark,
+  onSecondaryContainer: onSecondaryContainerDark,
+  tertiary: tertiaryDark,
+  onTertiary: onTertiaryDark,
+  tertiaryContainer: tertiaryContainerDark,
+  error: errorDark,
+  onError: onErrorDark,
+  background: backgroundDark,
+  onBackground: onBackgroundDark,
+  surface: surfaceDark,
+  onSurface: onSurfaceDark,
+  surfaceVariant: surfaceVariantDark,
+  onSurfaceVariant: onSurfaceVariantDark,
+  outline: outlineDark,
+  outlineVariant: outlineVariantDark,
+  shadow: shadowDark,
+  inverseSurface: inverseSurfaceDark,
+  inversePrimary: inversePrimaryDark,
+  surfaceTint: surfaceTintDark,
+);
